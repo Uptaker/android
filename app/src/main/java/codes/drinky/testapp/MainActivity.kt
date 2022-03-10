d20f64b91c2f214ffa8b0f0c1c589d079e9ed341
Uptaker <markus.tammeoja@gmail.com>
2022-03-10 13:05:16 +0200

    Add menu to switch between activity instances & add placeholder jokes activity window

--- app/src/main/java/codes/drinky/testapp/MainActivity.kt
+++ app/src/main/java/codes/drinky/testapp/MainActivity.kt
@@ -1,28 +1,38 @@
 package codes.drinky.testapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
-    private var timesClicked: Int = 0
+    private lateinit var openCounter: Button
+    private lateinit var openJokes: Button
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val counterBtn = findViewById<Button>(R.id.counterBtn)
-        val counter = findViewById<TextView>(R.id.counter)
-
-        counterBtn.setOnClickListener {
-            increment(counter)
-        }
+        openCounter = findViewById<Button>(R.id.openCounter)
+        openJokes = findViewById<Button>(R.id.openJokes)
+        setListeners()
     }
 
-    private fun increment(counter: TextView) {
-        timesClicked++
-        counter.text = timesClicked.toString()
+    private fun setListeners() {
+        openCounter.setOnClickListener { openCounterActivity() }
+        openJokes.setOnClickListener { openJokesActivity() }
     }
+
+    private fun openCounterActivity() {
+        val intent = Intent(this, CounterActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun openJokesActivity() {
+        val intent = Intent(this, JokesActivity::class.java)
+        startActivity(intent)
+    }
+
 }
