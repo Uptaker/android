34e25ff74b9f1cbadcf4c4a1dec468cb96fb6d73
Uptaker <markus.tammeoja@gmail.com>
2022-03-18 11:09:34 +0200

    Add accelerator layout & starter AcceleratorActivity.kt

---
+++ app/src/main/java/codes/drinky/testapp/AcceleratorActivity.kt
@@ -0,0 +1,11 @@
+package codes.drinky.testapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AcceleratorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_accelerator)
+    }
+}
