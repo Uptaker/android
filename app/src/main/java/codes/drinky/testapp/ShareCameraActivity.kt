14cc5d0fcf7b611ad42613d6b31fd0f27cc7f668
Uptaker <markus.tammeoja@gmail.com>
2022-04-09 21:46:57 +0300

    Add share from camera template

---
+++ app/src/main/java/codes/drinky/testapp/ShareCameraActivity.kt
@@ -0,0 +1,11 @@
+package codes.drinky.testapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ShareCameraActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_share_camera)
+    }
+}
