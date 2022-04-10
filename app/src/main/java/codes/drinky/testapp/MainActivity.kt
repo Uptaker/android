ad84abbdacd07a987a6156f31d0ffe82db5405ee
Uptaker <markus.tammeoja@gmail.com>
2022-04-10 23:03:55 +0300

    Too much changes to really count (too stressed for tiny commits).. Application now stores JSON, serializes, takes pictures, opens gallery (without doing anything w/ the pictures) does wacky bitmap/base64 conversions, coroutine hell and all this in one behemoth of a class!

--- app/src/main/java/codes/drinky/testapp/MainActivity.kt
+++ app/src/main/java/codes/drinky/testapp/MainActivity.kt
@@ -1,46 +1,261 @@
 package codes.drinky.testapp
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.widget.Button
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.FileProvider
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import codes.drinky.testapp.databinding.ActivityMainBinding
+import codes.drinky.testapp.model.Photo
+import codes.drinky.testapp.model.Upload
+import codes.drinky.testapp.model.Uploads
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import org.json.JSONException
+import org.json.JSONObject
+import org.json.JSONTokener
+import java.io.*
+import java.net.URL
+import java.util.*
+import javax.net.ssl.HttpsURLConnection
+
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var openHistory: Button
+    private lateinit var uploads: Uploads
+
     private lateinit var openCamera: Button
     private lateinit var openGallery: Button
 
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        uploads = getUploads()
+        setContentView(binding.root)
 
-        openHistory = findViewById<Button>(R.id.openHistory)
-        openCamera = findViewById<Button>(R.id.openCamera)
-        openGallery = findViewById<Button>(R.id.openGallery)
-        setListeners()
+        openCamera = findViewById(R.id.openCamera)
+        openGallery = findViewById(R.id.openGallery)
+        parseJson()
+
+        openCamera.setOnClickListener {
+            takeImage()
+        }
+        openGallery.setOnClickListener { loadImageFromGallery.launch("image/*") }
+
     }
 
-    private fun setListeners() {
-        openHistory.setOnClickListener { openHistoryActivity() }
-        openCamera.setOnClickListener { openCameraActivity() }
-        openGallery.setOnClickListener { openGalleryActivity() }
+    private val CLIENT_ID = BuildConfig.IMGUR_CLIENT_ID.toString()
+
+    private fun uploadImageToImgur(image: Bitmap) {
+        var shareLink: String
+        getBase64Image(image, complete = { base64Image ->
+            GlobalScope.launch(Dispatchers.Default) {
+                val url = URL("https://api.imgur.com/3/image")
+                val boundary = "Boundary-${System.currentTimeMillis()}"
+
+                val httpsURLConnection =
+                    withContext(Dispatchers.IO) { url.openConnection() as HttpsURLConnection }
+                httpsURLConnection.setRequestProperty("Authorization", "Client-ID $CLIENT_ID")
+                httpsURLConnection.setRequestProperty(
+                    "Content-Type",
+                    "multipart/form-data; boundary=$boundary"
+                )
+
+                httpsURLConnection.requestMethod = "POST"
+                httpsURLConnection.doInput = true
+                httpsURLConnection.doOutput = true
+
+                var body = ""
+                body += "--$boundary\r\n"
+                body += "Content-Disposition:form-data; name=\"image\""
+                body += "\r\n\r\n$base64Image\r\n"
+                body += "--$boundary--\r\n"
+
+
+                val outputStreamWriter = OutputStreamWriter(httpsURLConnection.outputStream)
+                withContext(Dispatchers.IO) {
+                    outputStreamWriter.write(body)
+                    outputStreamWriter.flush()
+                }
+
+                val response = httpsURLConnection.inputStream.bufferedReader()
+                    .use { it.readText() }  // defaults to UTF-8
+                val jsonObject = JSONTokener(response).nextValue() as JSONObject
+                shareLink = jsonObject.getJSONObject("data").getString("link")
+                pushToHistoryAfterUpload(shareLink)
+
+            }
+        })
     }
 
-    private fun openHistoryActivity() {
-        val intent = Intent(this, HistoryActivity::class.java)
-        startActivity(intent)
+    private fun getBase64Image(image: Bitmap, complete: (String) -> Unit) {
+        GlobalScope.launch {
+            val outputStream = ByteArrayOutputStream()
+            image.compress(Bitmap.CompressFormat.PNG, 100, outputStream)
+            val b = outputStream.toByteArray()
+            complete(Base64.getEncoder().encodeToString(b))
+        }
     }
 
-    private fun openGalleryActivity() {
-        val intent = Intent(this, ShareGalleryActivity::class.java)
-        startActivity(intent)
+    private suspend fun pushToHistoryAfterUpload(url: String) {
+        withContext(Dispatchers.Main) {
+            val capturedUpload = Upload(System.currentTimeMillis(), url)
+            uploads.uploads.add(0, capturedUpload)
+            parseJson()
+            writeToFile(UUID.randomUUID().toString(), Json.encodeToString(uploads))
+            copyToClipboard(url)
+        }
+
     }
 
-    private fun openCameraActivity() {
-        val intent = Intent(this, ShareCameraActivity::class.java)
-        startActivity(intent)
+    private var latestTmpUri: Uri? = null
+
+    private val takePhoto = registerForActivityResult(ActivityResultContracts.TakePicture()) { isSuccess ->
+        if (isSuccess) {
+            uploadImageToImgur(uriToBitmap(latestTmpUri!!))
+            Toast.makeText(this, "Uploading...", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this, "Did not upload", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
+    private fun uriToBitmap(uri: Uri): Bitmap {
+        return MediaStore.Images.Media.getBitmap(contentResolver, Uri.parse(uri.toString()))
+    }
+
+    private fun takeImage() {
+        lifecycleScope.launchWhenStarted {
+            getTmpFileUri().let { uri ->
+                latestTmpUri = uri
+                takePhoto.launch(uri)
+            }
+        }
+    }
+
+    private fun getTmpFileUri(): Uri {
+        val tmpFile = File.createTempFile("tmp_image_file", ".jpg", cacheDir).apply {
+            createNewFile()
+            deleteOnExit()
+        }
+
+        return FileProvider.getUriForFile(applicationContext, "${BuildConfig.APPLICATION_ID}.fileprovider", tmpFile)
+    }
+
+    private fun copyToClipboard(text: String) {
+        val clipboardManager = getSystemService(CLIPBOARD_SERVICE) as ClipboardManager
+        val clipData = ClipData.newPlainText("text", text)
+        println("Clip data: $text")
+        clipboardManager.setPrimaryClip(clipData)
+        Toast.makeText(this, "Success! Link copied to clipboard", Toast.LENGTH_SHORT).show()
+    }
+
+    private val loadImageFromGallery = registerForActivityResult(ActivityResultContracts.GetContent()) {
+        println(it)
+    }
+
+    private fun parseJson() {
+        try {
+            val uploadsView: RecyclerView = findViewById(R.id.uploadList)
+            uploadsView.layoutManager = LinearLayoutManager(this)
+            val itemAdapter = UploadAdapter(this, uploads.uploads)
+            uploadsView.adapter = itemAdapter
+        } catch (e: JSONException) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun remove(date: Long, uploads: Uploads): Uploads {
+        uploads.uploads.removeIf { it.uploadDate == date }
+        return uploads
+    }
+
+    private fun writeToFile(fileName: String, content: String) {
+        val path = applicationContext.filesDir
+        val file = File(path, "uploads.json")
+        file.createNewFile()
+        FileOutputStream(file).use {
+            it.write(content.toByteArray())
+        }
+    }
+
+    private fun getUploads(): Uploads {
+        val uploadsJson = readFile()
+        println(uploadsJson)
+        return if (uploadsJson != null) {
+            Json.decodeFromString(uploadsJson)
+        } else {
+            Uploads(arrayListOf())
+        }
+    }
+
+    private fun readFile(): String? {
+        return try {
+            val file = File(applicationContext.filesDir, "uploads.json")
+            val contents = file.readText()
+            contents
+        } catch (e: Exception) {
+            e.printStackTrace()
+            null
+        }
+    }
+
+    private fun savePhoto(fileName: String, bmp: Bitmap): Boolean {
+        return try {
+            openFileOutput("$fileName.jpg", MODE_PRIVATE).use { stream ->
+                if (!bmp.compress(Bitmap.CompressFormat.JPEG, 95, stream)) {
+                    throw IOException("Couldn't save bitmap")
+                }
+            }
+            true
+        } catch (e: IOException) {
+            e.printStackTrace()
+            false
+        }
+    }
+
+    private fun loadPhotosToRecyclerView() {
+        lifecycleScope.launch {
+            val photos = loadPhotos()
+            println(photos)
+        }
+    }
+
+    private suspend fun loadPhotos(): List<Photo> {
+        return withContext(Dispatchers.IO) {
+            val files = filesDir.listFiles()
+            files?.filter { it.canRead() && it.isFile && it.name.endsWith(".jpg") }?.map {
+                val bytes = it.readBytes()
+                val bmp = BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
+                Photo(it.name, bmp)
+            } ?: listOf()
+        }
+    }
+
+    private fun deletePhoto(fileName: String): Boolean {
+        return try {
+            deleteFile(fileName)
+        } catch (e: Exception) {
+            e.printStackTrace()
+            false
+        }
     }
 
 }
