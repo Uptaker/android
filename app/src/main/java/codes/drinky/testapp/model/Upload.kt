87df0694b5a90c2fc3c8b236f4a2fe5aecfe005e
Uptaker <markus.tammeoja@gmail.com>
2022-04-10 23:00:51 +0300

    Add serializable upload and uploads data classes

--- app/src/main/java/codes/drinky/testapp/model/Upload.kt
+++ app/src/main/java/codes/drinky/testapp/model/Upload.kt
@@ -1,6 +1,14 @@
 package codes.drinky.testapp.model
 
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Uploads(
+    val uploads: ArrayList<Upload>
+)
+
+@Serializable
 data class Upload(
     val uploadDate: Long,
-    var url: String,
+    val url: String,
 )
