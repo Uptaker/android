6b8bfaae72e33da726f394be610a37aa869deaad
Uptaker <markus.tammeoja@gmail.com>
2022-04-13 23:18:04 +0300

    Make image openable in browser and move copy functionality to copy icon

--- app/src/main/java/codes/drinky/testapp/UploadAdapter.kt
+++ app/src/main/java/codes/drinky/testapp/UploadAdapter.kt
@@ -1,13 +1,16 @@
 package codes.drinky.testapp
 
 import android.content.Context
+import android.content.Intent
 import android.graphics.Bitmap
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
 import codes.drinky.testapp.client.ImgurClient
 import codes.drinky.testapp.model.Upload
@@ -18,6 +21,8 @@ import kotlin.collections.ArrayList
 
 class UploadAdapter(private val context: Context, private val items: ArrayList<Upload>) :
     RecyclerView.Adapter<UploadAdapter.ViewHolder>() {
+
+    private val defaultBrowser: Intent = Intent.makeMainSelectorActivity(Intent.ACTION_MAIN, Intent.CATEGORY_APP_BROWSER)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         return ViewHolder(
@@ -49,6 +54,7 @@ class UploadAdapter(private val context: Context, private val items: ArrayList<U
         val date: TextView = view.findViewById(R.id.tv_upload_date)
         val url: TextView = view.findViewById(R.id.tv_url)
         val image: ImageView = view.findViewById(R.id.imageView)
+        private val copy: ImageView = view.findViewById(R.id.copy)
 
         init {
             view.setOnLongClickListener {
@@ -58,9 +64,15 @@ class UploadAdapter(private val context: Context, private val items: ArrayList<U
                 true
             }
 
-            view.setOnClickListener {
+            copy.setOnClickListener {
                 val position: Int = adapterPosition
                 (context as MainActivity).copyToClipboard(items[position].url)
+            }
+
+            image.setOnClickListener {
+                val position: Int = adapterPosition
+                defaultBrowser.data = Uri.parse(items[position].url)
+                (context as MainActivity).startActivity(defaultBrowser)
             }
         }
     }
